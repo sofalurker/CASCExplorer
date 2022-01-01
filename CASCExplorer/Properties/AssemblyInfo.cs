0cc64ca425b07a278c7be37f79fa031ad3aed16a
TOM_RUS <tomrus88@gmail.com>
2022-01-01 14:32:34 +0300

    Fix

--- CASCExplorer/Properties/AssemblyInfo.cs
+++ CASCExplorer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CASCExplorer")]
-[assembly: AssemblyCopyright("Copyright © TOM_RUS 2014-2020")]
+[assembly: AssemblyCopyright("Copyright © TOM_RUS 2014-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
